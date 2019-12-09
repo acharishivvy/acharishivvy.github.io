43dab53dce17cef24d2fbfd585add7d1270fa489
SaiyanShivvy <hdiproductionsoffical@gmail.com>
2019-12-09 14:33:40 +1300

    Added Github Project/Links.

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,8 +1,8 @@
 <?php
-// Check for empty fields
+/** Check for empty fields */
 if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-  http_response_code(500);
-  exit();
+    http_response_code(500);
+    exit();
 }
 
 $name = strip_tags(htmlspecialchars($_POST['name']));
@@ -11,11 +11,11 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
-$to = "saiyanshivvy@yahoo.co.nz"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = "acharishivvy@outlook.com"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
 $subject = "Website Contact Form:  $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
 $header = "From: noreply@saiyanshivvy.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$header .= "Reply-To: $email";	
+$header .= "Reply-To: $email";
 
 if(!mail($to, $subject, $body, $header))
   http_response_code(500);
