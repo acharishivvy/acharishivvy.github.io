37ed55146d6effc92e21d66c11208dd9e84a5c20
Shivneel Achari <acharishivvy@yahoo.co.nz>
2022-04-18 07:06:16 +1200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === "production";
+const isProd = (process.env.NODE_ENV || "production") === "production";
 
 const nextConfig = {
 	reactStrictMode: true,
+	exportPathMap: () => ({
+		"/": { page: "/" },
+	}),
+	basePath: isProd ? "/saiyanshivvy.github.io/" : "",
 	assetPrefix: isProd ? "/saiyanshivvy.github.io/" : "",
 	images: {
 		loader: "imgix",
