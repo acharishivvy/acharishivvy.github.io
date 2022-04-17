539b2ad7b311e81cdab21b47add030b090fb28e5
Shivneel Achari <acharishivvy@yahoo.co.nz>
2022-04-18 05:56:25 +1200

    Update ci.yaml, gh-pages.deploy.yml, and next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
-const basePath = process.env.NODE_ENV === "production" ? "/repo" : "";
 const nextConfig = {
 	reactStrictMode: true,
-	basePath,
-	assetPrefix: `${basePath}/`,
 	images: {
 		loader: "imgix",
 		path: "",
