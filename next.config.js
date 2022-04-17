519a678fdd1ea6a47d412debe1d881c0d5c050ef
Shivneel Achari <acharishivvy@yahoo.co.nz>
2022-04-18 06:07:52 +1200

    Update gh-pages.deploy.yml and next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+const main = process.env.DEPLOY_TARGET === "main";
+
 const nextConfig = {
 	reactStrictMode: true,
+	assetPrefix: main ? "/saiyanshivvy.github.io/" : "",
 	images: {
 		loader: "imgix",
 		path: "",
