04289f15dea9ae72f55520754792439717b277c3
Shivneel Achari <acharishivvy@yahoo.co.nz>
2022-04-20 21:47:37 +1200

    Updated

--- pages/api/sendgrid.js
+++ pages/api/sendgrid.js
@@ -38,7 +38,6 @@ async function sendEmail(req, res) {
       </html>`,
 		});
 	} catch (error) {
-		// console.log(error);
 		return res.status(error.statusCode || 500).json({ error: error.message });
 	}
 
