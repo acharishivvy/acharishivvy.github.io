c179e43c2047e9e5fd3b676edb6d40bafb4be691
Shivneel Achari <acharishivvy@yahoo.co.nz>
2022-04-22 00:00:32 +1200

    3

--- next.config.js
+++ next.config.js
@@ -4,15 +4,11 @@ const isProd = (process.env.NODE_ENV || "production") === "production";
 
 const nextConfig = {
 	reactStrictMode: true,
-	exportPathMap: () => ({
-		"/": { page: "/" },
-	}),
 	basePath: "",
 	images: {
 		loader: "imgix",
 		path: "",
 	},
-	webpack5: true,
 };
 
 module.exports = nextConfig;
