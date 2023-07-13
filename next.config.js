e3069e1dd1d684d0792b62fdf29f944d059a7fa6
Shivneel Achari <acharishivvy@yahoo.co.nz>
2023-07-13 14:46:16 +1200

    Update Github Actions, Config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+	output: "export",
 	reactStrictMode: true,
 	basePath: "",
 	images: {
-		loader: "imgix",
-		path: "",
+		unoptimized: true,
 	},
 };
 
