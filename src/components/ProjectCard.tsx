46aa33a3c0b92a64fe22d38a0e01688fe0ae449e
Shivneel Achari <acharishivvy@yahoo.co.nz>
2025-02-25 17:58:25 +1300

    Updating Layout, Working on Carousel and Contact Form.

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -3,7 +3,14 @@ import React from "react";
 import { Button } from "@/components/ui/button";
 import { Dialog } from "@/components/ui/dialog";
 import Image from "next/image";
-import { Card, CardDescription, CardHeader, CardTitle } from "./ui/card";
+import {
+	Card,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+	CardFooter,
+	CardContent,
+} from "./ui/card";
 import { Label } from "./ui/label";
 export default function ProjectCard({
 	title,
@@ -20,37 +27,48 @@ export default function ProjectCard({
 	imageUrl: string;
 }) {
 	return (
-		<Card className='m-4'>
-			<Image
-				src={imageUrl}
-				alt={title}
-				width={200}
-				height={150}
-				className='mr-4'
-			/>
-			<CardHeader>
-				<CardTitle>{title}</CardTitle>
-				<CardDescription>{description}</CardDescription>
-			</CardHeader>
-
-			{technologies && (
-				<div className='mt-2'>
-					<h3>Technologies Used:</h3>
-					<ul>
-						{technologies.map((tech, index) => (
-							<Label key={index}>{tech}</Label>
-						))}
-					</ul>
+		<Card className='w-full max-w-lg shadow-none'>
+			<CardHeader className='flex flex-col py-2'>
+				<div className='flex gap-3'>
+					<div className='flex flex-col gap-4'>
+						<h6 className='text-sm leading-none font-medium'>{title}</h6>
+					</div>
 				</div>
-			)}
+			</CardHeader>
+			<CardContent className='p-0'>
+				<div className='relative border-y' />
+				<Image
+					src={imageUrl}
+					alt={title}
+					width={400}
+					height={350}
+					className='mt-4 rounded-lg object-cover'
+				/>
+				<div className='pt-3 pb-4 px-6'>
+					<h2 className='font-semibold'>Description</h2>
+					<p className='mt-1 text-sm text-muted-foreground'>{description}</p>
+				</div>
+			</CardContent>
+			<CardFooter className=' py-2 px-2'>
+				{technologies && (
+					<div>
+						<h3 className='text-sm font-semibold'>Technologies Used:</h3>
+						<ul className='mt-1 space-y-1'>
+							{technologies.map((tech, index) => (
+								<Label key={index} className='w-full text-muted-foreground'>
+									{tech}
+								</Label>
+							))}
+						</ul>
+					</div>
+				)}
 
-			{githubLink && (
-				<Dialog>
+				{githubLink && (
 					<a href={githubLink} target='_blank' rel='noopener noreferrer'>
 						<Button variant='outline'>View on GitHub</Button>
 					</a>
-				</Dialog>
-			)}
+				)}
+			</CardFooter>
 		</Card>
 	);
 }
--- src/components/ProjectCarousel.tsx
+++ src/components/ProjectCarousel.tsx
@@ -18,7 +18,7 @@ export function VerticalProjectCarousel() {
 			description:
 				"This is another brief description. It provides an overview of what the project does.",
 			technologies: ["React", "Vite", "TailwindCSS", "Shadcn"],
-			githubLink: "https://github.com/SaiyanShivvy/tw-react-dex",
+			githubLink: "https://github.com/acharishivvy/tw-react-dex",
 			liveLink: "https://neo-tw-react-dex.vercel.app/",
 			imageUrl: "reactdex.png",
 		},
@@ -28,7 +28,7 @@ export function VerticalProjectCarousel() {
 			description:
 				"I tried to create a simple platformer game using libGDX that could be run on android. The Project is incomplete.",
 			technologies: ["libGDX 1.9.8", "Java"],
-			githubLink: "https://github.com/SaiyanShivvy/libgdx-platformer",
+			githubLink: "https://github.com/acharishivvy/libgdx-platformer",
 			imageUrl: "libgdx.gif",
 		},
 		{
@@ -37,7 +37,7 @@ export function VerticalProjectCarousel() {
 			description:
 				"The aim of the project is to recreate the classic snake game using C++ and SFML, with the ability to add another player. The Snake is controlled using WSAD (or Directional Keys). Collect Apple (Red Squares) to grow your snake. Eating yourself ends the game.",
 			technologies: ["C++", "C", "SFML"],
-			githubLink: "https://github.com/SaiyanShivvy/sfml-snake-game",
+			githubLink: "https://github.com/acharishivvy/sfml-snake-game",
 			imageUrl: "sfmlsnake.gif",
 		},
 		{
@@ -46,7 +46,7 @@ export function VerticalProjectCarousel() {
 			description:
 				"This project is meant to demonstrate the use of Super, Base Classes and various ViewForm Elements. It is done in the form of a simple GPS where the user is able to add a 'object' to the 'map' by inputting various types of data including Building Type, Location, Rating.",
 			technologies: ["C#"],
-			githubLink: "https://github.com/SaiyanShivvy/Pseudo-GPS",
+			githubLink: "https://github.com/acharishivvy/Pseudo-GPS",
 			imageUrl: "phpqs.png",
 		},
 		{
@@ -55,7 +55,7 @@ export function VerticalProjectCarousel() {
 			description:
 				"Simple E-Commerce Website using C# ASP.NET Core and the MVC Architecture. The User's are able to a register, add to cart and checkout items from the store. There are are also predefined admin user's that are able to create, update or delete item listing. They can also check and manage user order's.",
 			technologies: ["C#", "ASP.NET MVC", "ASP.NET Core"],
-			githubLink: "https://github.com/SaiyanShivvy/Quality-Souvenirs",
+			githubLink: "https://github.com/acharishivvy/Quality-Souvenirs",
 			imageUrl: "phpqs.png",
 		},
 		{
@@ -64,22 +64,22 @@ export function VerticalProjectCarousel() {
 			description:
 				"The task was to re-create the C# E-Commerce Website by using PHP and Laravel instead.",
 			technologies: ["Laravel 5.2.0", "Composer for PHP", "Node.js", "XAMPP"],
-			githubLink: "https://github.com/SaiyanShivvy/phpQS",
+			githubLink: "https://github.com/acharishivvy/phpQS",
 			imageUrl: "ViewForm1.jpg",
 		},
 	];
 
 	return (
-		<Carousel className='w-full max-w-xs'>
-			<CarouselContent className='-mt-1 h-[400px]'>
-				{projects.map((project) => (
-					<CarouselItem key={project.id} className='md:basis-1/2'>
-						<ProjectCard {...project} />
-					</CarouselItem>
-				))}
-			</CarouselContent>
-			<CarouselPrevious />
-			<CarouselNext />
-		</Carousel>
+			<Carousel className='max-w-4xl'>
+				<CarouselContent>
+					{projects.map((project) => (
+						<CarouselItem key={project.id} className="p-1">
+							<ProjectCard {...project} />
+						</CarouselItem>
+					))}
+				</CarouselContent>
+				<CarouselPrevious className="" />
+				<CarouselNext className="" />
+			</Carousel>
 	);
 }
